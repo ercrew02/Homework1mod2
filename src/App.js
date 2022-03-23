d71c001c354e8ab040675f51beb0b1d96b68418b
Erlangga <erlangga.sukmana02@gmail.com>
2022-03-23 13:40:41 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./App.css";
+import data from "./data";
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={data.album.images[2].url} />
+      <h1>{data.name}</h1>
+      <h2>Penyanyi: {data.artists[0].name}</h2>
+      <br />
+      <br />
+      <button type="button">Select</button>
     </div>
   );
 }
-
-export default App;
